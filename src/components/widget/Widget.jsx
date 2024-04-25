cea2a89101a92b854953f8b99af03ca8a5461f07
DangDucThuan38 <dangducthuan999@gmail.com>
2024-04-25 12:53:26 +0700

    update

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,15 +1,12 @@
 import "./widget.css";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
 
 const Widget = ({ type, amount }) => {
     let data;
 
     //temporary
-    const diff = 20;
 
     switch (type) {
         case "user":
@@ -26,6 +23,8 @@ const Widget = ({ type, amount }) => {
                 ),
             };
             break;
+        default:
+                break;
     }
 
     return (
